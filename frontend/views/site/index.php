fddcb58de6831f601e52faf758dd0344ac17cebe
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2019-07-13 18:56:02 +0300

    fix: correction of layout (div in the wrong place)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,6 +11,7 @@ $this->title = Yii::$app->name;
 ?>
 <div class="site-index">
     <div class="body-content">
+        
         <div class="row">
             <div class="col-12">
                 <div class="page-header">
@@ -18,27 +19,30 @@ $this->title = Yii::$app->name;
                 </div>
             </div>
         </div>
+
         <div class="row">
             <div class="col-12">
                 <div class="page-header">
                     <h2>Task 1</h2>
-                    <div class="well">
-                        <p>Используя Yii2 необходимо реализовать форму регистрации пользователя с условием типа
-                            физ./юр.
-                            лицо.</p>
-                        <p>Для физ. лица необходимо заполнить: почту, ФИО и в случае ИП - ИНН, а для юр. лица:
-                            почту,
-                            ФИО, название организации и инн.</p>
-                        <p>Внешний вид значения не имеет.</p>
-                    </div>
+                </div>
+                <div class="well">
+                    <p>Используя Yii2 необходимо реализовать форму регистрации пользователя с условием типа
+                        физ./юр.
+                        лицо.</p>
+                    <p>Для физ. лица необходимо заполнить: почту, ФИО и в случае ИП - ИНН, а для юр. лица:
+                        почту,
+                        ФИО, название организации и инн.</p>
+                    <p>Внешний вид значения не имеет.</p>
+                </div>
+                <div class="page-header">
                     <h2>Solution</h2>
-                    <div class="well">
-                        <p>Создано 2 формы с выбором физ./юр. лица, переключение по табам.</p>
-                        <p>В зависимости от выбора, в каждой форме есть скрытый параметр type с boolean 0/1 физ./юр.
-                            лицо
-                            соответственно</p>
-                        <p>Валидация ИНН и Организации проходит на сервере.</p>
-                    </div>
+                </div>
+                <div class="well">
+                    <p>Создано 2 формы с выбором физ./юр. лица, переключение по табам.</p>
+                    <p>В зависимости от выбора, в каждой форме есть скрытый параметр type с boolean 0/1 физ./юр.
+                        лицо
+                        соответственно</p>
+                    <p>Валидация ИНН и Организации проходит на сервере.</p>
                     <p>Страница с формой регистрации:
                         <?= Html::a('Signup', Url::to(['/site/signup']), ['class' => "btn btn-xs btn-link"]) ?></p>
                 </div>
@@ -49,10 +53,11 @@ $this->title = Yii::$app->name;
             <div class="col-12">
                 <div class="page-header">
                     <h2>Task 2</h2>
-                    <div class="well">
-                        <p>Реализовать кеширование для функции:</p>
-                        <?php
-                        $codeTask2 = '<?php
+                </div>
+                <div class="well">
+                    <p>Реализовать кеширование для функции:</p>
+                    <?php
+                    $codeTask2 = '<?php
 function ($date, $type)
 {
     $userId = Yii::$app->user->id;
@@ -67,23 +72,25 @@ function ($date, $type)
     }
     return $result;
 }';
-                        try {
-                            $highlighted = $highlighter->highlight('php', $codeTask2);
-                            echo "<pre><code class=\"hljs {$highlighted->language}\">";
-                            echo $highlighted->value;
-                            echo "</code></pre>";
-                        } catch (Exception $e) {
-                            echo "<pre><code>";
-                            echo $codeTask2;
-                            echo "</code></pre>";
-                        }
-                        ?>
+                    try {
+                        $highlighted = $highlighter->highlight('php', $codeTask2);
+                        echo "<pre><code class=\"hljs {$highlighted->language}\">";
+                        echo $highlighted->value;
+                        echo "</code></pre>";
+                    } catch (Exception $e) {
+                        echo "<pre><code>";
+                        echo $codeTask2;
+                        echo "</code></pre>";
+                    }
+                    ?>
 
-                    </div>
+                </div>
+                <div class="page-header">
                     <h2>Solution</h2>
-                    <div class="well">
-                        <?php
-                        $codeSolution2 = 'public static function getSomeData($date, $type)
+                </div>
+                <div class="well">
+                    <?php
+                    $codeSolution2 = 'public static function getSomeData($date, $type)
 {
     $cache = Yii::$app->cache;
 
@@ -113,28 +120,28 @@ function ($date, $type)
     }
     return $result;
 }';
-                        try {
-                            $highlighted = $highlighter->highlight('php', $codeSolution2);
-                            echo "<pre><code class=\"hljs {$highlighted->language}\">";
-                            echo $highlighted->value;
-                            echo "</code></pre>";
-                        } catch (Exception $e) {
-                            echo "<pre><code>";
-                            echo $codeSolution2;
-                            echo "</code></pre>";
-                        }
-                        ?>
-                        <p>
-                            Ссылка на код функции:
-                            <a href="https://github.com/superrosko/regru-yii2/blob/71ef8ccb4eabb6c7e5e0d0c0e1e8db222cc40f57/frontend/models/SomeDataModel.php#L27">
-                                getSomeData
-                            </a>
-                        </p>
-                        <p>Реализовано не через getOrSet, чтобы минимально менять код функции и не через кеширование
-                            запросов, чтобы закешировать уже результирующий массив.</p>
-                    </div>
+                    try {
+                        $highlighted = $highlighter->highlight('php', $codeSolution2);
+                        echo "<pre><code class=\"hljs {$highlighted->language}\">";
+                        echo $highlighted->value;
+                        echo "</code></pre>";
+                    } catch (Exception $e) {
+                        echo "<pre><code>";
+                        echo $codeSolution2;
+                        echo "</code></pre>";
+                    }
+                    ?>
+                    <p>
+                        Ссылка на код функции:
+                        <a href="https://github.com/superrosko/regru-yii2/blob/71ef8ccb4eabb6c7e5e0d0c0e1e8db222cc40f57/frontend/models/SomeDataModel.php#L27">
+                            getSomeData
+                        </a>
+                    </p>
+                    <p>Реализовано не через getOrSet, чтобы минимально менять код функции и не через кеширование
+                        запросов, чтобы закешировать уже результирующий массив.</p>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
